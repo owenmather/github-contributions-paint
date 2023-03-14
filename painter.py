2d6680bcc99bd4df3c1aa4ccd7104de8516a343a
owenmather <owenmather7@gmail.com>
2023-03-14 15:22:09 +0100

    Add github painter

---
+++ painter.py
@@ -0,0 +1,104 @@
+import tkinter
+import os
+
+WHITE = "white"
+INPUT_IMAGE_PATH = os.environ.get("INPUT_IMAGE_PATH", "image.txt")
+
+color0 = "#161b22"
+color1 = "#0e4429"
+color2 = "#006d32"
+color3 = "#26a641"
+color4 = "#39d353"
+
+colors = [color0, color1, color2, color3, color4]
+
+next_color = {
+    color0: color1,
+    color1: color2,
+    color2: color3,
+    color3: color4,
+    color4: color0
+}
+
+
+class Painter:
+    canvas: tkinter.Canvas
+
+    def __init__(self, margin=20, square_size=30, horizontal_squares=20, vertical_squares=7):
+        self.MARGIN = margin
+        self.SQUARE_SIZE = square_size
+        self.HORIZONTAL_SQUARES = horizontal_squares
+        self.VERTICAL_SQUARES = vertical_squares
+        self.HEIGHT = self.VERTICAL_SQUARES * self.SQUARE_SIZE + self.MARGIN * 2
+        self.WIDTH = self.HORIZONTAL_SQUARES * self.SQUARE_SIZE + self.MARGIN * 2
+        self.grid = []
+
+    def get_square(self, event):
+        return int((event.x - self.MARGIN) / self.SQUARE_SIZE) + ((int((event.y - self.MARGIN) / self.SQUARE_SIZE)) *
+                                                                  self.HORIZONTAL_SQUARES)
+
+    def change_square_color(self, event):
+        target = self.get_square(event)
+        color = next_color[self.canvas.itemconfig(self.grid[target])['fill'][-1]]
+        self.canvas.itemconfig(self.grid[target], fill=color)
+
+    def clear_grid(self):
+        for rect in self.grid:
+            self.canvas.itemconfig(rect, fill=color0)
+
+    def save_grid(self):
+        image_data = ""
+        for idx, rect in enumerate(self.grid):
+            if idx > 0 and idx % self.HORIZONTAL_SQUARES == 0:
+                image_data += "\n"
+            image_data += str(colors.index(self.canvas.itemconfig(rect)['fill'][-1]))
+
+        with open(INPUT_IMAGE_PATH, "w") as csv_file:
+            csv_file.write(image_data)
+
+    def load_grid(self):
+        self.clear_grid()
+        with open(INPUT_IMAGE_PATH, "r") as image_file:
+            data = image_file.read().split("\n")
+            data = "".join(data)
+
+        for idx, code in enumerate(list(data)):
+            self.canvas.itemconfig(self.grid[idx], fill=colors[int(code)])
+
+    def run(self):
+        top = tkinter.Tk()
+        top.resizable(False, False)
+        self.canvas = tkinter.Canvas(top, bg=WHITE, height=self.HEIGHT, width=self.WIDTH)
+
+        for y in range(7):
+            for i in range(self.HORIZONTAL_SQUARES):
+                rect = self.canvas.create_rectangle((i * self.SQUARE_SIZE) + self.MARGIN,
+                                                    (y * self.SQUARE_SIZE) + self.MARGIN,
+                                                    ((i + 1) * self.SQUARE_SIZE) + self.MARGIN,
+                                                    ((y + 1) * self.SQUARE_SIZE) + self.MARGIN,
+                                                    fill=color0,
+                                                    outline=WHITE, tags="change_square_color")
+                self.grid.append(rect)
+                self.canvas.tag_bind('change_square_color', "<Button-1>", self.change_square_color)
+        # Code to add widgets will go here...
+        self.canvas.pack()
+        frame = tkinter.Frame(top)
+        frame.pack()
+        # flat, groove, raised, ridge, solid, or sunken
+        load_button = tkinter.Button(frame, text="Load", command=self.load_grid, fg=WHITE, bg="blue", width=10,
+                                     height=2, relief="raised", bd=1)
+        clear_button = tkinter.Button(frame, text="Clear", command=self.clear_grid, fg=WHITE, bg="red", width=10,
+                                      height=2, relief="raised", bd=1)
+        save_button = tkinter.Button(frame, text="Save", command=self.save_grid, width=10, height=2, relief="raised",
+                                     bd=1)
+
+        load_button.pack({"side": "left"})
+        clear_button.pack({"side": "left"})
+        save_button.pack({"side": "left"})
+
+        top.mainloop()
+
+
+if __name__ == '__main__':
+    p = Painter()
+    p.run()
---
+++ readme.md
@@ -0,0 +1,37 @@
+## GitHub Contributions Painter
+
+Paints a bit image on the `(inf)*7` grid for GitHub Contributions
+
+### Details
+
+Uses the GitHub actions workflow in [draw.yml](.github/workflows/draw.yml) to commit to repo
+
+1) Runs `main.py` to get the commit count for the current day   
+This is determined by offset into `image.txt`  
+     
+2) Commits a bump the required times to `bump` file and pushes to same repo  
+   Uses the [GITHUB_TOKEN](https://docs.github.com/en/actions/security-guides/automatic-token-authentication#about-the-github_token-secret) from the actions workflow to push the changes
+
+### Painter GUI 
+
+Users can use the helper **painter.py** GUI to draw bit images for printing
+
+```
+python painter.py
+```
+
+This GUI supports the following functionality:
+
+> **Draw:**  Color squares by clicking. 5 color levels supported  
+**Load:**  Load the most recent image from `INPUT_IMAGE_PATH`
+**Clear:** Clear the grid  
+**Save:**  Save the image to `INPUT_IMAGE_PATH`
+
+
+![GUI Example](readme_images/img_1.png)
+
+### Environment Variables
+
+| Key | Value | Desc |
+| --- | ---| ---|
+| INPUT_IMAGE_PATH | `image.txt` | The path to save/load/generate the bit image from |
